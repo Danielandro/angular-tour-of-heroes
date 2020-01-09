b39111ca6a248e38800ba16c45cfd7a640d0aeb0
Danielandro <mtme13@hotmail.com>
2020-01-09 17:47:56 +0000

    Navigating to hero details

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -8,21 +8,19 @@ import { HeroService } from "../hero.service";
   styleUrls: ["./heroes.component.css"]
 })
 export class HeroesComponent implements OnInit {
-  selectedHero: Hero;
   heroes: Hero[];
 
   // inject service into constructor
   constructor(private heroService: HeroService) {}
 
   ngOnInit() {
-    // subscribe to returned observable to access heroes
+    // get heroes from service
+    this.getHeroes();
+  }
+
+  getHeroes(): void {
     this.heroService.getHeroes().subscribe(heroes => {
       this.heroes = heroes;
     });
-  }
-
-  // set selected hero
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
   }
 }
